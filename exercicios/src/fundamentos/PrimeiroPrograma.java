6e7cc32d54c8fabb58eff03293e88dbc779e0ada
Ricardo Wegner <ricardowegneroliveira@gmail.com>
2023-09-04 12:40:15 -0400

    Dica: Não encher o código com comentários desnecessários

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,5 +1,12 @@
 package fundamentos;
 
+/**
+ * 
+ * @author Ricardo <email@gmail.com>
+ * @Since 1.0
+ * @see google.com
+ */
+
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		System.out.println("Primeiro programa!!!");
